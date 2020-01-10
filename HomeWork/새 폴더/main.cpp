4026ce0b08136c9a4dc766d273c1f68e13c6bd56
MinjI <42130179+jmj627a@users.noreply.github.com>
2020-01-10 22:36:56 +0900

    homework7_슈팅게임
    파일에서 몬스터 배열 읽어오기

--- HomeWork/새 폴더/main.cpp
+++ HomeWork/새 폴더/main.cpp
@@ -1,17 +1,9 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include <memory.h>
 #include <Windows.h>
 #include "Console.h"
 
-enum GameSceneType
-{
-	TITLE = 0,
-	INGAME = 1,
-	GAMEOVER = 2,
-	LOADING = 3
-};
+
 
 struct PLAYER
 {
@@ -34,135 +26,90 @@ struct BULLET
 	int y;
 };
 
-//struct STAGE_FILE
-//{
-//	unsigned int stage;
-//	unsigned int data
-//};
+ENEMY g_Enemy[20];
 
-ENEMY g_Enemy[100];
 BULLET g_Bullet[100];
-PLAYER g_player;
 
-int g_GameSceneType = LOADING;// INGAME;
-int g_GameStage = 0;
-int g_TotalStage = 3;
-
-void playerInit()
-{
-	g_player.x = 40;
-	g_player.y = 20;
-}
-
-void enemyInit()
-{
-	FILE* pStageFile = fopen("Enemy.txt", "rb");
-
-	fseek(pStageFile, 0, SEEK_END);
-	int size = ftell(pStageFile);
-
-	char* pStageBuffer = (char*)malloc(size/g_TotalStage);
-	rewind(pStageFile);
-	
-	//fseek(pStageFile, 1889 * g_GameStage, SEEK_SET);
-	//int temp = fread(pStageBuffer, 1, 1, pStageFile);
-	//
-	//char temp22 = *pStageBuffer;
-	//printf("%c\n",temp22);
-
-	//1886 = 80*23 + 2*23			82 * 23
-	fseek(pStageFile, 3 * (g_GameStage+1) + g_GameStage * 1886, SEEK_SET);
-
-	int temp = fread(pStageBuffer, 1, 1886, pStageFile);
-	pStageBuffer[1887] = '\0';
-
-	printf("%s", pStageBuffer);
-
-
-	int count = 0;
-	for (int i = 0; i < 1886; ++i)
-	{
-		if (pStageBuffer[i] == '7')
-		{
-			g_Enemy[count].flag = true;
-			g_Enemy[count].x = i % 82;
-			g_Enemy[count].y = i / 82;
-
-			count++;
-		}
-	}
-	
-	system("pause");
-}
 
 //--------------------------------------------------------------------
 // 화면 깜빡임을 없애기 위한 화면 버퍼.
+// 게임이 진행되는 상황을 매번 화면을 지우고 비행기 찍고, 지우고 찍고,
+// 하게 되면 화면이 깜빡깜빡 거리게 된다.
+//
+// 그러므로 화면과 똑같은 크기의 메모리를 할당한 다음에 화면에 바로 찍지않고
+// 메모리(버퍼)상에 그림을 그리고 메모리의 화면을 그대로 화면에 찍어준다.
+//
+// 이렇게 해서 화면을 매번 지우고, 그리고, 지우고, 그리고 하지 않고
+// 메모리(버퍼)상의 그림을 화면에 그리는 작업만 하게 되어 깜박임이 없어진다.
+//
+// 버퍼의 각 줄 마지막엔 NULL 을 넣어 문자열로서 처리하며, 
+// 한줄한줄을 printf 로 찍어나갈 것이다.
+//
+// for ( N = 0 ~ height )
+// {
+// 	  cs_MoveCursor(0, N);
+//    printf(szScreenBuffer[N]);
+// }
+//
+// 줄바꿈에 printf("\n") 을 쓰지 않고 커서좌표를 이동하는 이유는
+// 화면을 꽉 차게 출력하고 줄바꿈을 하면 2칸이 내려가거나 화면이 밀릴 수 있으므로
+// 매 줄 출력마다 좌표를 강제로 이동하여 확실하게 출력한다.
 //--------------------------------------------------------------------
 char szScreenBuffer[dfSCREEN_HEIGHT][dfSCREEN_WIDTH];
 
 
 //--------------------------------------------------------------------
 // 버퍼의 내용을 화면으로 찍어주는 함수.
+//
+// 적군,아군,총알 등을 szScreenBuffer 에 넣어주고, 
 // 1 프레임이 끝나는 마지막에 본 함수를 호출하여 버퍼 -> 화면 으로 그린다.
 //--------------------------------------------------------------------
 void Buffer_Flip(void);
-
-
 //--------------------------------------------------------------------
 // 화면 버퍼를 지워주는 함수
+//
+// 매 프레임 그림을 그리기 직전에 버퍼를 지워 준다. 
+// 안그러면 이전 프레임의 잔상이 남으니까
 //--------------------------------------------------------------------
 void Buffer_Clear(void);
 
 //--------------------------------------------------------------------
 // 버퍼의 특정 위치에 원하는 문자를 출력.
+//
+// 입력 받은 X,Y 좌표에 아스키코드 하나를 출력한다. (버퍼에 그림)
 //--------------------------------------------------------------------
 void Sprite_Draw(int iX, int iY, char chSprite);
 
 
 
 //키보드 입력
-void KeyInput()
+void KeyInput(PLAYER* player)
 {
 	if (GetAsyncKeyState(VK_LEFT))
 	{
-		g_player.x -= 1;
+		player->x  -= 1;
 	}
 	else if (GetAsyncKeyState(VK_RIGHT))
 	{
-		g_player.x += 1;
+		player->x += 1;
 	}
 	else if (GetAsyncKeyState(VK_UP))
 	{
-		g_player.y -= 1;
+		player->y -= 1;
 	}
 	else if (GetAsyncKeyState(VK_DOWN))
 	{
-		g_player.y += 1;
+		player->y += 1;
 	}
 }
 
 
-void Game_Scene()
-{
-	//Render
-	// 스크린 버퍼를 지움
-	Buffer_Clear();
 
-	// 스크린 버퍼에 스프라이트 출력 
-	Sprite_Draw(g_player.x, g_player.y, 'A');
-
-	// 스크린 버퍼를 화면으로 출력
-	Buffer_Flip();
-
-	//키입력
-	KeyInput();
-
-	//로직처리
-}
 
 
 void main(void)
 {
+	PLAYER player;
 
 	cs_Initial();
 	Buffer_Clear();
@@ -173,25 +120,22 @@ void main(void)
 	
 	while (1)
 	{
-		switch (g_GameSceneType)
-		{
-		case TITLE:
+		//Render
+		// 스크린 버퍼를 지움
+		Buffer_Clear();
 
-			break;
+		// 스크린 버퍼에 스프라이트 출력 
+		Sprite_Draw(player.x, player.y, 'A');
 
-		case INGAME:
+		// 스크린 버퍼를 화면으로 출력
+		Buffer_Flip();
 
-			break;
 
-		case GAMEOVER:
 
-			break;
+		//키입력
+		KeyInput(&player);
 
-		case LOADING:
-			playerInit();
-			enemyInit();
-			break;
-		}
+		//로직처리
 
 
 		// 프레임 맞추기용 대기 10 Frame
