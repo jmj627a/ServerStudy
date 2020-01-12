9ee1aaef4131fe9f032982037c44baf1690d599b
정민지 <jmj627a@gmail.com>
2020-01-13 03:22:23 +0900

    콘솔 슈팅 게임 - 몬스터 출력, 스테이지 변경(f12로 1,2,3) , 플레이어 총알 발사

--- HomeWork/homework8_ShootingGame/homework8_ShootingGame/main.cpp
+++ HomeWork/homework8_ShootingGame/homework8_ShootingGame/main.cpp
@@ -5,6 +5,12 @@
 #include <Windows.h>
 #include "Console.h"
 
+#define ENEMY_NUM 100
+#define BULLET_NUM 100
+
+#define PLAYER_TYPE 0
+#define ENEMY_TYPE 1
+
 enum GameSceneType
 {
 	TITLE = 0,
@@ -30,18 +36,16 @@ struct ENEMY
 
 struct BULLET
 {
+	bool flag;
+	bool type; // PLAYER_TYPE 0 , ENEMY_TYPE 1
+
 	int x;
 	int y;
 };
 
-//struct STAGE_FILE
-//{
-//	unsigned int stage;
-//	unsigned int data
-//};
 
-ENEMY g_Enemy[100];
-BULLET g_Bullet[100];
+ENEMY* g_Enemy;
+BULLET g_Bullet[BULLET_NUM];
 PLAYER g_player;
 
 int g_GameSceneType = LOADING;// INGAME;
@@ -56,6 +60,9 @@ void playerInit()
 
 void enemyInit()
 {
+	free(g_Enemy);
+	g_Enemy = (ENEMY*)malloc(sizeof(ENEMY) * ENEMY_NUM);
+	
 	FILE* pStageFile = fopen("Enemy.txt", "rb");
 
 	fseek(pStageFile, 0, SEEK_END);
@@ -76,7 +83,7 @@ void enemyInit()
 	int temp = fread(pStageBuffer, 1, 1886, pStageFile);
 	pStageBuffer[1887] = '\0';
 
-	printf("%s", pStageBuffer);
+	//printf("%s", pStageBuffer);
 
 
 	int count = 0;
@@ -92,56 +99,157 @@ void enemyInit()
 		}
 	}
 	
-	system("pause");
+	//system("pause");
 }
 
-//--------------------------------------------------------------------
+void bulletInit()
+{
+	//free(g_Bullet);
+	//g_Bullet = (BULLET*)malloc(sizeof(BULLET) * BULLET_NUM);
+
+	for (int i = 0; i < BULLET_NUM; ++i)
+	{
+		g_Bullet[i].flag = false;
+	}
+}
+
+
 // 화면 깜빡임을 없애기 위한 화면 버퍼.
-//--------------------------------------------------------------------
 char szScreenBuffer[dfSCREEN_HEIGHT][dfSCREEN_WIDTH];
 
 
-//--------------------------------------------------------------------
 // 버퍼의 내용을 화면으로 찍어주는 함수.
-// 1 프레임이 끝나는 마지막에 본 함수를 호출하여 버퍼 -> 화면 으로 그린다.
-//--------------------------------------------------------------------
 void Buffer_Flip(void);
 
 
-//--------------------------------------------------------------------
 // 화면 버퍼를 지워주는 함수
-//--------------------------------------------------------------------
 void Buffer_Clear(void);
 
-//--------------------------------------------------------------------
 // 버퍼의 특정 위치에 원하는 문자를 출력.
-//--------------------------------------------------------------------
-void Sprite_Draw(int iX, int iY, char chSprite);
+void Draw_Player(int iX, int iY, char chSprite);
+void Draw_Enemy(char chSprite);
+void Draw_Bullet(char chSprite);
 
 
+//넘겨주는 씬으로 변경 
+void Change_Scene(int type)
+{
+	switch (type)
+	{
+	case TITLE:		
+		g_GameSceneType = TITLE;	break;
+
+	case INGAME:
+		g_GameSceneType = INGAME;	break;
+
+	case GAMEOVER:
+		g_GameSceneType = GAMEOVER;		break;
+
+	case LOADING:
+		g_GameSceneType = LOADING;		break;
+	}
+}
+
+//누구 총알인지, 몬스터면 몇번째 몬스터한테 나온 총알인지
+void create_bullet(bool type, int num)
+{
+	for (int i = 0; i < BULLET_NUM; ++i)
+	{
+		if (false == g_Bullet[i].flag)
+		{
+			switch (type)
+			{
+			case PLAYER_TYPE:
+				g_Bullet[i].x = g_player.x;
+				g_Bullet[i].y = g_player.y;
+				g_Bullet[i].flag = true;
+				g_Bullet[i].type = PLAYER_TYPE;
+				break;
+
+			case ENEMY_TYPE:
+				g_Bullet[i].x = g_Enemy[num].x;
+				g_Bullet[i].y = g_Enemy[num].y;
+				g_Bullet[i].flag = true;
+				g_Bullet[i].type = ENEMY_TYPE;
+				break;
+			}
+
+			return;
+		}
+	}
+}
 
 //키보드 입력
 void KeyInput()
 {
 	if (GetAsyncKeyState(VK_LEFT))
 	{
-		g_player.x -= 1;
+		if(g_player.x > 0)
+			g_player.x -= 1;
 	}
-	else if (GetAsyncKeyState(VK_RIGHT))
+	if (GetAsyncKeyState(VK_RIGHT))
 	{
-		g_player.x += 1;
+		if (g_player.x < dfSCREEN_WIDTH - 2)
+			g_player.x += 1;
 	}
-	else if (GetAsyncKeyState(VK_UP))
+	if (GetAsyncKeyState(VK_UP))
 	{
-		g_player.y -= 1;
+		if (g_player.y > 0)
+			g_player.y -= 1;
 	}
-	else if (GetAsyncKeyState(VK_DOWN))
+	if (GetAsyncKeyState(VK_DOWN))
 	{
-		g_player.y += 1;
+		if (g_player.y < dfSCREEN_HEIGHT - 1)
+			g_player.y += 1;
+	}
+	if (GetAsyncKeyState(VK_SPACE))
+	{
+		create_bullet(PLAYER_TYPE, 0);
+	}
+	if (GetAsyncKeyState(VK_F12))
+	{
+		g_GameStage = (g_GameStage + 1) % g_TotalStage;
+		Change_Scene(LOADING);
 	}
 }
 
+void bullet_Move()
+{
+	for (int i = 0; i < BULLET_NUM; ++i)
+	{
+		if (true == g_Bullet[i].flag)
+		{
+			switch (g_Bullet[i].type)
+			{
+			case PLAYER_TYPE:
+				g_Bullet[i].y--;
+				break;
 
+			case ENEMY_TYPE:
+				g_Bullet[i].y++;
+				break;
+			}
+		}
+	}
+}
+
+//총알 , 벽
+void collision_Bullet_Wall()
+{
+	for (int i = 0; i < BULLET_NUM; ++i)
+	{
+		if (true == g_Bullet[i].flag)
+		{
+			if (g_Bullet[i].y < 0)
+				g_Bullet[i].flag = false;
+
+			if (g_Bullet[i].y >= dfSCREEN_HEIGHT)
+				g_Bullet[i].flag = false;
+		}
+	}
+}
+
+//게임씬
 void Game_Scene()
 {
 	//Render
@@ -149,7 +257,9 @@ void Game_Scene()
 	Buffer_Clear();
 
 	// 스크린 버퍼에 스프라이트 출력 
-	Sprite_Draw(g_player.x, g_player.y, 'A');
+	Draw_Player(g_player.x, g_player.y, 'A');
+	Draw_Enemy('M');
+	Draw_Bullet('O');
 
 	// 스크린 버퍼를 화면으로 출력
 	Buffer_Flip();
@@ -158,18 +268,18 @@ void Game_Scene()
 	KeyInput();
 
 	//로직처리
+	//enemy_move();
+	bullet_Move();
+
+	collision_Bullet_Wall();
 }
+
 
 
 void main(void)
 {
-
 	cs_Initial();
 	Buffer_Clear();
-	//--------------------------------------------------------------------
-	// 게임의 메인 루프
-	// 이 루프가  1번 돌면 1프레임 이다.
-	//--------------------------------------------------------------------
 	
 	while (1)
 	{
@@ -180,7 +290,7 @@ void main(void)
 			break;
 
 		case INGAME:
-
+			Game_Scene();
 			break;
 
 		case GAMEOVER:
@@ -190,6 +300,8 @@ void main(void)
 		case LOADING:
 			playerInit();
 			enemyInit();
+			bulletInit();
+			Change_Scene(INGAME);
 			break;
 		}
 
@@ -201,12 +313,7 @@ void main(void)
 
 
 
-//--------------------------------------------------------------------
 // 버퍼의 내용을 화면으로 찍어주는 함수.
-//
-// 적군,아군,총알 등을 szScreenBuffer 에 넣어주고, 
-// 1 프레임이 끝나는 마지막에 본 함수를 호출하여 버퍼 -> 화면 으로 그린다.
-//--------------------------------------------------------------------
 void Buffer_Flip(void)
 {
 	//cs_ClearScreen();
@@ -214,17 +321,23 @@ void Buffer_Flip(void)
 	for (int i = 0; i < dfSCREEN_HEIGHT; ++i)
 	{
 		cs_MoveCursor(0, i);
-		printf("%s", szScreenBuffer[i]);
+		printf("%s\n", szScreenBuffer[i]);
 	}
+
+	
+	//그냥 창 테두리
+	for (int i = 0; i < dfSCREEN_HEIGHT; ++i)
+	{
+		cs_MoveCursor(dfSCREEN_WIDTH - 2, i);
+		printf("│");
+	}
+
+	cs_MoveCursor(0, dfSCREEN_HEIGHT);
+	printf("─────────────────────────────────────────────────────────────────────────────────");
 }
 
 
-//--------------------------------------------------------------------
 // 화면 버퍼를 지워주는 함수
-//
-// 매 프레임 그림을 그리기 직전에 버퍼를 지워 준다. 
-// 안그러면 이전 프레임의 잔상이 남으니까
-//--------------------------------------------------------------------
 void Buffer_Clear(void)
 {
 	for (int i = 0; i < dfSCREEN_HEIGHT; ++i)
@@ -242,12 +355,28 @@ void Buffer_Clear(void)
 //
 // 입력 받은 X,Y 좌표에 아스키코드 하나를 출력한다. (버퍼에 그림)
 //--------------------------------------------------------------------
-void Sprite_Draw(int iX, int iY, char chSprite)
+void Draw_Player(int iX, int iY, char chSprite)
 {
 	szScreenBuffer[iY][iX] = chSprite;
+}
 
+void Draw_Enemy(char chSprite)
+{
+	for (int i = 0; i < ENEMY_NUM; ++i)
+	{
+		if(true == g_Enemy[i].flag)
+			szScreenBuffer[g_Enemy[i].y][g_Enemy[i].x] = chSprite;
+	}
 }
 
 
-
-
+void Draw_Bullet(char chSprite)
+{
+	for (int i = 0; i < BULLET_NUM; ++i)
+	{
+		if (true == g_Bullet[i].flag)
+		{
+			szScreenBuffer[g_Bullet[i].y][g_Bullet[i].x] = chSprite;
+		}
+	}
+}
