fd5f63e0f3fb7aea83058bab185d4f76861bfda2
정민지 <jmj627a@gmail.com>
2020-01-19 00:33:57 +0900

    homework10_template, linkedlist 이용해서 new delete 메모리 누수 찾기

--- HomeWork/homework10/homework10/homework10/소스.cpp
+++ HomeWork/homework10/homework10/homework10/소스.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <iostream>
 
 #define _MemoryAlloc(type, num) 	 MemoryAlloc<type>(num,  __LINE__, __FILE__) 
 
@@ -16,59 +17,123 @@ struct AllocInfo
 	AllocInfo* next;
 };
 
-AllocInfo* info = (AllocInfo*)malloc(sizeof(AllocInfo));
+AllocInfo* info;
 
 template <typename T>
-void listAdd(int* temp, int num, int _LineNum, const char* _FileName)
+void listAdd(T* temp, int num, int _LineNum, const char* _FileName)
 {
+	AllocInfo* newNode = (AllocInfo*)malloc(sizeof(AllocInfo));
+
+	//헤드일때 헤드 아닐떄
 	if (info == NULL)
 	{
-		info->ptr = temp;
-		info->size = num;
-		info->FileLine = _LineNum;
-		strcpy(info->FileName, _FileName);
+		newNode->ptr = temp;
+		newNode->size = num * sizeof(T);
+		newNode->FileLine = _LineNum;
+		strcpy(newNode->FileName, _FileName);
+		newNode->next = NULL;
+
+		info = newNode;
+		return;
+	}
+	else
+	{
+		AllocInfo* t = info;
+		while (t->next != NULL)
+		{
+			t = t->next;
+		}
+
+		newNode->ptr = temp;
+		newNode->size = num * sizeof(T);
+		newNode->FileLine = _LineNum;
+		strcpy(newNode->FileName, _FileName);
+		newNode->next = NULL;
+
+		t->next = newNode;
+		return;
 	}
 }
-
 
 template <typename T>
 T* MemoryAlloc(int _num, int _LineNum, const char* _FileName)
 {
 	T* temp = new T[_num];
 
-
-	listAdd<T>(temp, _num, _LineNum, _FileName);
-
+	listAdd(temp, _num, _LineNum, _FileName);
 
 	return temp;
 }
 
-template <typename T>
-void MemoryRelease(T* a)
+void deleteNode(AllocInfo* Node)
 {
+	AllocInfo* temp = Node;
+	AllocInfo* delNode = Node->next;
 
+	temp->next = temp->next->next;
+	delete delNode;
+}
+
+template <typename T>
+bool MemoryRelease(T* a)
+{
+	AllocInfo* temp = info;
+	T* findPtr = a;
+
+	if (temp->ptr == findPtr)
+	{
+		info = info->next;
+		delete temp;
+		return true;
+	}
+
+	while (temp->next != NULL)
+	{
+		if (temp->next->ptr == findPtr)
+		{
+			deleteNode(temp);
+			return true;
+		}
+		temp = temp->next;
+	}
+
+	return false;
 }
 
 void MemoryPrint()
 {
 	AllocInfo* fi = info;
-	while (fi->next != NULL)
+	if (fi == NULL)
+		return;
+
+	while (true)
 	{
-		printf("ptr 주소 : %d \n", fi->ptr);
-		printf("size : %d \n", fi->size);
+		printf("ptr 주소 : %X \n", (fi->ptr));
+		printf("size : %d byte \n", fi->size);
 		printf("위치 : %d \n", fi->FileLine);
-		printf("이름 : %s \n", fi->FileName);
+		printf("이름 : %s \n\n", fi->FileName);
 
 
 		fi = fi->next;
+		if (fi == NULL)
+			break;
 	}
 }
 
 
 void main()
 {
-	int* p = _MemoryAlloc(int, 10);
 
+	char* p = _MemoryAlloc(char, 5);
+	int* p1 = _MemoryAlloc(int, 10);
+	double *p2 = _MemoryAlloc(double, 15);
+	double *p3 = _MemoryAlloc(double, 100);
+	double *p4 = _MemoryAlloc(double, 100);
+
+	MemoryRelease((int*)0x12341234);
+	MemoryRelease(p2);
+	MemoryRelease(p1);
+	MemoryRelease(p);
 
 	MemoryPrint();
 }
--- HomeWork/homework10/homework10/homework10/소스1.cpp
+++ HomeWork/homework10/homework10/homework10/소스1.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <iostream>
 
 #define _MemoryAlloc(type, num) 	 MemoryAlloc(num,  __LINE__, __FILE__) 
 
@@ -22,6 +23,7 @@ void listAdd(int* temp, int num, int _LineNum, const char* _FileName)
 {
 	AllocInfo* newNode = (AllocInfo*)malloc(sizeof(AllocInfo));
 
+	//헤드일때 헤드 아닐떄
 	if (info == NULL)
 	{
 		newNode->ptr = temp;
@@ -35,9 +37,10 @@ void listAdd(int* temp, int num, int _LineNum, const char* _FileName)
 	}
 	else
 	{
-		while (info->next != NULL)
+		AllocInfo* t = info;
+		while (t->next != NULL)
 		{
-			info = info->next;
+			t = t->next;
 		}
 
 		newNode->ptr = temp;
@@ -46,7 +49,7 @@ void listAdd(int* temp, int num, int _LineNum, const char* _FileName)
 		strcpy(newNode->FileName, _FileName);
 		newNode->next = NULL;
 
-		info->next = newNode;
+		t->next = newNode;
 		return;
 	}
 }
@@ -56,27 +59,57 @@ int* MemoryAlloc(int _num, int _LineNum, const char* _FileName)
 {
 	int* temp = new int[_num];
 
-
 	listAdd(temp, _num, _LineNum, _FileName);
-
 
 	return temp;
 }
 
-void MemoryRelease(int* a)
+void deleteNode(AllocInfo* Node)
 {
+	AllocInfo* temp = Node;
+	AllocInfo* delNode = Node->next;
 
+	temp->next = temp->next->next;
+	delete delNode;
+}
+
+bool MemoryRelease(int* a)
+{
+	AllocInfo* temp = info;
+	int* findPtr = a;
+
+	if (temp->ptr == findPtr)
+	{
+		info = info->next;
+		delete temp;
+		return true;
+	}
+
+	while (temp->next != NULL)
+	{
+		if (temp->next->ptr == findPtr)
+		{
+			deleteNode(temp);
+			return true;
+		}
+		temp = temp->next;
+	}
+
+	return false;
 }
 
 void MemoryPrint()
 {
 	AllocInfo* fi = info;
+	if (fi == NULL)
+		return;
+
 	while (true)
 	{
-		printf("ptr 주소 : %d \n", fi->ptr);
+		printf("ptr 주소 : %X \n", (fi->ptr));
 		printf("size : %d byte \n", fi->size);
 		printf("위치 : %d \n", fi->FileLine);
-		printf("이름 : %s \n", fi->FileName);
+		printf("이름 : %s \n\n", fi->FileName);
 
 
 		fi = fi->next;
@@ -89,9 +122,14 @@ void MemoryPrint()
 void main()
 {
 
-	int* p = _MemoryAlloc(int, 10);
-	int* p1 = _MemoryAlloc(int, 5);
+	int* p = _MemoryAlloc(int, 5);
+	int* p1 = _MemoryAlloc(int, 10);
+	int* p2 = _MemoryAlloc(int, 15);
 
+	MemoryRelease((int*)0x12341234);
+	MemoryRelease(p2);
+	MemoryRelease(p1);
+	MemoryRelease(p);
 
 	MemoryPrint();
 }
